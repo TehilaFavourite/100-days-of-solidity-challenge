3e71e931142329b1ff5a4d21d607fe1c8d96fffb
tehilafavourite <tehilafavourite@gmail.com>
2023-05-02 20:55:46 +0100

    first commit

---
+++ contracts/Day 1/README.md
@@ -0,0 +1,13 @@
+# 100-days-of-solidity
+
+# Day 1: Introduction to Solidity
+
+-Understand what Solidity is and its importance in blockchain development
+
+Solidity is an object-oriented, high-level language for implementing smart contracts. Smart contracts are programs which govern the behaviour of accounts within the Ethereum state.
+
+Solidity is a curly-bracket language designed to target the Ethereum Virtual Machine (EVM). It is influenced by C++, Python and JavaScript. You can find more details about which languages Solidity has been inspired by in the language influences section.
+
+Solidity is a powerful language that enables developers to create smart contracts and interact with the Ethereum network. It is a vital tool for building decentralized applications and implementing blockchain-based solutions.
+
+Solidity is statically typed, supports inheritance, libraries and complex user-defined types among other features.
---
+++ contracts/Day 2-3: Solidity Basics/datatypes.sol
@@ -0,0 +1,52 @@
+pragma solidity 0.8.19;
+
+contract datatypes {
+    // Boolean
+    bool public isTrue = true;
+
+    // integer
+    int8 public smallNumber = -128;
+    int256 public bigNumber = 2 ** 255 - 1;
+
+    // Unsigned integer
+    uint8 public smallUnsignedNumber = 255;
+    uint256 public bigUnsignedNumber = 2 ** 256 - 1;
+
+    // Address
+    address public myAddress = address(this);
+
+    // String
+    string public myString = "Hello, world!";
+
+    // Bytes
+    bytes public myBytes = hex"1234567890abcdef";
+
+    // Fixed-point decimal
+    //  NOte: avoid using fixed-point numbers
+    fixed public fixedNumber = 1.5;
+    ufixed public ufixedNumber = 0.123;
+
+    // Enum
+    enum State {
+        Pending,
+        Approved,
+        Rejected
+    }
+    State public currentState = State.Pending;
+
+    // Array
+    uint256[] public myArray = [1, 2, 3, 4, 5];
+
+    // Mapping
+    mapping(address => uint256) public myMapping;
+
+    // Struct
+    struct Person {
+        string name;
+        uint256 age;
+    }
+    Person public myPerson = Person("Alice", 30);
+
+    // Contract
+    DataTypesDemo public myContract = this;
+}
