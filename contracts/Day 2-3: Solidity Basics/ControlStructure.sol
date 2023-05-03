887f8fdafd82463e2956a799ca32361e86040a6e
tehilafavourite <tehilafavourite@gmail.com>
2023-05-03 13:27:35 +0100

    day 2 and 3 of 100 days of solidity

--- contracts/Day 2-3: Solidity Basics/ControlStructure.sol
+++ contracts/Day 2-3: Solidity Basics/ControlStructure.sol
@@ -39,5 +39,72 @@ contract ControlStructure {
                 break; // this means we finished the loop at "i == 5"
             }
         }
+        // while loop
+        uint A = 0;
+        while (while A < 10) {
+            // code
+            j++;
+        }
+    }
+
+    function add(uint b) external pure returns (uint) {
+        uint y;
+        for (i = 0; i <= b; i++;) {
+            y += i;
+        }
+        return y;
+    }
+}
+
+// more examples
+
+contract ControlStructuresDemo {
+    // if statement
+    function isEven(uint256 num) public pure returns (bool) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // for loop
+    function sum(uint256[] memory nums) public pure returns (uint256) {
+        uint256 total = 0;
+        for (uint256 i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        return total;
+    }
+
+    // while loop
+    function factorial(uint256 num) public pure returns (uint256) {
+        uint256 result = 1;
+        while (num > 1) {
+            result *= num;
+            num--;
+        }
+        return result;
+    }
+
+    // do-while loop
+    function count(uint256 num) public pure returns (uint256) {
+        uint256 count = 0;
+        do {
+            count++;
+        } while (count < num);
+        return count;
+    }
+
+
+    // ternary operator
+    function isPositive(int256 num) public pure returns (bool) {
+        return num > 0 ? true : false;
+    }
+
+    // require statement
+    function divide(uint256 x, uint256 y) public pure returns (uint256) {
+        require(y != 0, "division by zero");
+        return x / y;
     }
 }
