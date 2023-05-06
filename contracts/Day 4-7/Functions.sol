bb87802c002fec1f375c61b14ef1ed7f95f77813
tehilafavourite <tehilafavourite@gmail.com>
2023-05-06 23:33:35 +0100

    day 4 to 7

--- contracts/Day 4-7/Functions.sol
+++ contracts/Day 4-7/Functions.sol
@@ -24,3 +24,19 @@ contract FunctionVisibility {
 
 // pure and view function
 
+pragma solidity ^0.8.0;
+
+contract Function {
+    uint public myNumber;
+
+// this is a view function because it reads data (myNumber) from the blockchain
+    function getNumber() public view returns (uint) {
+        return myNumber; 
+    }
+
+// This is a pure function because it does not read anything from the blockchain
+    function addNumbers(uint a, uint b) public pure returns (uint) {
+        return a + b;
+    }
+}
+
