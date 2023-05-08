74e9a818cfb87c15ca8a2c30ee9346c75cfff76d
tehilafavourite <tehilafavourite@gmail.com>
2023-05-08 21:27:16 +0100

    day 4 to 7

--- contracts/Day 4-7/Functions.sol
+++ contracts/Day 4-7/Functions.sol
@@ -61,3 +61,30 @@ contract Function {
     }
 }
 
+// function modifier
+pragma solidity 0.8.19;
+
+contract MyContract {
+    address public owner;
+    uint public myNumber;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only contract owner can call this function");
+        _; // This indicates where the function body will be placed
+    }
+
+    modifier validNumber {
+        require(myNumber > 0, "Number must be greater than zero");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        myNumber = 0;
+    }
+
+    function setNumber(uint _number) public onlyOwner validNumber {
+        myNumber = _number;
+    }
+}
+
