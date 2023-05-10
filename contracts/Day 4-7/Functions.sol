c3137e9ff1e15c46d03b580bc4ce1a63615d1649
tehilafavourite <tehilafavourite@gmail.com>
2023-05-10 13:42:11 +0100

    day 4 to 7

--- contracts/Day 4-7/Functions.sol
+++ contracts/Day 4-7/Functions.sol
@@ -88,6 +88,20 @@ contract MyContract {
     }
 }
 
+// modifier overriding
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.19;
+
+contract Base
+    {
+    modifier foo() virtual {_;}
+    }
+    
+contract Inherited is Base
+    {
+    modifier foo() override {_;}
+    }
+
 
 
 // more examples
