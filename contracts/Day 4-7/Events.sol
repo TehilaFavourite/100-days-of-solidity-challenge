07777db3354e4842eb2df774b9e8a4005065ae7c
tehilafavourite <tehilafavourite@gmail.com>
2023-05-12 23:02:54 +0100

    day 4 to 7

---
+++ contracts/Day 4-7/Events.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract EventExample {
+    event NewValue(uint256 indexed newValue);
+
+    uint256 public value;
+
+    function updateValue(uint256 _newValue) public {
+        value = _newValue;
+        emit NewValue(_newValue);
+    }
+}
