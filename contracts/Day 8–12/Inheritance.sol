bc698b2a9d5c4157a7fe0ce0e907f9b2bd6f2c6b
tehilafavourite <tehilafavourite@gmail.com>
2023-05-23 23:28:34 +0100

    day 8 to 12

---
+++ contracts/Day 8–12/Inheritance.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract House {
+    // Base contract defining common features of a house
+    // ...
+
+    // House-specific functions and variables
+    // ...
+}
+
+contract SmartHouse is House {
+    // Specialized contract inheriting from House
+    // ...
+
+    // SmartHouse-specific functions and variables
+    // ...
+}
