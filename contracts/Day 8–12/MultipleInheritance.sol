ca9f708edd092104889fa905bd433d18a2cc47c8
tehilafavourite <tehilafavourite@gmail.com>
2023-05-23 23:30:31 +0100

    day 8 to 12

---
+++ contracts/Day 8–12/MultipleInheritance.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract SeniorDevA {
+    function solidityTut() public pure returns (string memory) {
+        return "I teach solidity!";
+    }
+}
+
+contract SeniorDevB {
+    function testingSmartContracts() public pure returns (string memory) {
+        return "I can teach smart contract testing!";
+    }
+}
+
+contract SeniorDevC {
+    function yulOpcodes() public pure returns (string memory) {
+        return "I can teach yul opcodes!";
+    }
+}
+
+contract Skilss is SeniorDevA, SeniorDevB, SeniorDevC {
+    // Superhero-specific functions and variables
+    // ...
+}
+
+
