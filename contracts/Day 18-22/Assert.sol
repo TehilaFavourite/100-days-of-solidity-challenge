e2b2ffb3f1df81c3be8f40970b90ad3e4dc4bf1d
tehilafavourite <tehilafavourite@gmail.com>
2023-07-18 11:19:20 +0100

    100 days of solidity

---
+++ contracts/Day 18-22/Assert.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AssertExample {
+    uint256 public balance = 1000;
+
+    function withdraw(uint256 amount) public {
+        // Ensure the contract has sufficient balance before withdrawing
+        assert(balance >= amount);
+
+        // Deduct the withdrawn amount from the balance
+        balance -= amount;
+    }
+}
